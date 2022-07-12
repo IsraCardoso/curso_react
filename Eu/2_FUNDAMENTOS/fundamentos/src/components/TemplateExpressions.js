46158efbc1402f2a465e889734590b2d1c343608
IsraCardoso <israelalmeidacardoso@gmail.com>
2022-07-12 13:49:17 -0300

    templateexpressions

--- Eu/2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
+++ Eu/2_FUNDAMENTOS/fundamentos/src/components/TemplateExpressions.js
@@ -1,9 +1,15 @@
 const TemplateExpressions = () =>{
   const name = 'Israel'
+  const data={
+    age:28,
+    job:'Software Engineer'
+  }
 
   return(
     <div>
       <h1>Olá {name}, tudo bem?</h1>
+      <p>Você atua como {data.job}</p>
+      <p>Idade: {data.age}</p>
     </div>
   )
 }
