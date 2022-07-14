7f890da159033f69fe171c0fff8ab640d54234df
IsraCardoso <israelalmeidacardoso@gmail.com>
2022-07-14 17:12:37 -0300

    Css react

---
+++ Eu/4_CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import  "./MyComponent.css"
+
+const MyComponent = () => {
+  return (
+    <>
+      <h1>CSS de componente</h1>
+      <p>Esse faz parte do componente</p>
+    </>
+  )
+}
+
+export default MyComponent
---
+++ Eu/4_CSS_REACT/css/src/components/Title.js
@@ -0,0 +1,9 @@
+import styles from './Title.module.css'
+
+const Title = () => {
+  return (
+    <h1 className={styles.my_style}>CSS modules</h1>
+  )
+}
+
+export default Title
